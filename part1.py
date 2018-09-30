e2cb06112bdc49b242579e2104fdd41f6c80c67e
Adam James Barillas <adam.barillas@gmail.com>
2018-09-30 19:02:24 +0200

    Small change

--- part1.py
+++ part1.py
@@ -2,8 +2,8 @@ import nltk
 
 from nltk.tokenize import word_tokenize, sent_tokenize
 
-example = 'Hello Mr. Smith, how are you doing today? The weather is awesome. The waiter is blueish-pink. I like to eat ' \
-          'cardboard. Thank you very much.'
+example = 'Hello Mr. Smith, how are you doing today? The weather is awesome. The water is blueish-pink. I like to eat ' \
+          'pizza. Thank you very much.'
 
 print(sent_tokenize(example))
 print(word_tokenize(example))
